8485f84f0a1049307cc9685d0d8a094f60641119
Agung Sunarya <sunaryaagung95@gmail.com>
2019-08-21 12:07:01 +0700

    [Arya] add documentation

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,11 +2,12 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 
 module.exports = function(req, res, next) {
-  const token = req.header("x-auth-token");
+  let token = req.header("x-auth-token");
   if (!token) return res.status(401).send("Access denied no token provided.");
+  let tokenSplit = token.split(" ");
 
   try {
-    const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+    let decoded = jwt.verify(tokenSplit[1], config.get("jwtPrivateKey"));
     req.user = decoded;
     next();
   } catch (ex) {
--- startup/routes.js
+++ startup/routes.js
@@ -2,12 +2,15 @@ const express = require("express");
 const login = require("../routes/login");
 const users = require("../routes/users");
 const posts = require("../routes/posts");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("../swagger.json");
 
 module.exports = function(app) {
   app.use(express.json());
-  app.use("/api/logn", login);
+  app.use("/api/login", login);
   app.use("/api/users", users);
   app.use("/api/posts", posts);
+  app.use("/api/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.get("/", async (req, res) => {
     res.send("Root");
   });
